f11e78bd104422c23f9a743844098d22b672ca8c
conny <conny.carlberg@gmail.com>
2018-02-18 15:32:56 +0100

    changes required due to other ip for node - plus mismatch version etcd och kuberenetes 1.9

--- scripts/etcd-crd.sh
+++ scripts/etcd-crd.sh
@@ -4,12 +4,6 @@ echo "installing etcd operator"
 kubectl  create -f https://raw.githubusercontent.com/coreos/etcd-operator/master/example/deployment.yaml
 kubectl  rollout status -f https://raw.githubusercontent.com/coreos/etcd-operator/master/example/deployment.yaml
 
-until kubectl  get thirdpartyresource cluster.etcd.coreos.com
-do
-    echo "waiting for operator"
-    sleep 2
-done
-
 echo "pausing for 10 seconds for operator to settle"
 sleep 10
 
